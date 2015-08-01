c14bc2c654870b1ec40b1398c4b426391ac5356e
Brian Young <bdy-github@hiddentower.com>
2015-08-01 10:55:47 -0700

    Initial Project Setup
    Default iOS single view project setup (in Swift)

---
+++ GeekSpeak Show TimerTests/GeekSpeak_Show_TimerTests.swift
@@ -0,0 +1,36 @@
+//
+//  GeekSpeak_Show_TimerTests.swift
+//  GeekSpeak Show TimerTests
+//
+//  Created by Brian Cordan Young on 8/1/15.
+//  Copyright (c) 2015 Brian Young. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GeekSpeak_Show_TimerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
