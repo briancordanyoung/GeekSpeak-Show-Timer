4bfa7d67e2230680cb2a2d746ba1892435ae0b7e
Brian Young <bdy-github@hiddentower.com>
2015-08-02 21:49:05 -0700

    adding my own standard library

---
+++ GeekSpeak Show Timer/BCYStandardLibrary.swift
@@ -0,0 +1,35 @@
+
+
+func comments(() -> ()) {
+    // This is a no-op function created to use the trailing closure syntax
+    // to wrap up a bunch of comments for 
+}
+
+// See: http://owensd.io/2015/05/12/optionals-if-let.html
+extension Optional {
+ 
+  var hasValue: Bool {
+    get {
+      return hasValue(self)
+    }
+  }
+  
+  func hasValue<T>(value: T?) -> Bool {
+    switch (value) {
+    case .Some(_): return true
+    case .None:    return false
+    }
+  }
+  
+  var hasNoValue: Bool {
+    get {
+      return hasNoValue(self)
+    }
+  }
+
+  func hasNoValue<T>(value: T?) -> Bool {
+    let result = hasValue(value)
+    return !result
+  }
+
+}
