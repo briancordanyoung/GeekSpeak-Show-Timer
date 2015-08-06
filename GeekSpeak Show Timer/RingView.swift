923386adaf49a9c60fb7d55b0b60d22d33054b28
Brian Young <bdy-github@hiddentower.com>
2015-08-05 22:24:23 -0700

    Created coders and decoders for both RingView and PieShapeView Classes

--- GeekSpeak Show Timer/RingView.swift
+++ GeekSpeak Show Timer/RingView.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class RingView: FillView {
   
+  struct Constants {
+    static let LineWidth  = "ringViewLineWidthId"
+    static let ColorRed   = "ringViewColorRedId"
+    static let ColorGreen = "ringViewColorGreenId"
+    static let ColorBlue  = "ringViewColorBlueId"
+    static let ColorAlpha = "ringViewColorAlphaId"
+  }
+  
   convenience init() {
     self.init(frame: CGRectMake(0,0,100,100))
     lineWidth = 15.0
@@ -16,9 +24,30 @@ class RingView: FillView {
   
   required init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
-    // TODO: impliment coder
-//    lineWidth = 15.0
-//    lineColor = UIColor.redColor()
+    lineWidth = CGFloat(aDecoder.decodeDoubleForKey(Constants.LineWidth))
+    let r = CGFloat(aDecoder.decodeDoubleForKey(Constants.ColorRed))
+    let g = CGFloat(aDecoder.decodeDoubleForKey(Constants.ColorGreen))
+    let b = CGFloat(aDecoder.decodeDoubleForKey(Constants.ColorBlue))
+    let a = CGFloat(aDecoder.decodeDoubleForKey(Constants.ColorAlpha))
+    lineColor = UIColor(red: r, green: g, blue: b, alpha: a)
+  }
+  
+  override func encodeWithCoder(aCoder: NSCoder) {
+    super.encodeWithCoder(aCoder)
+    aCoder.encodeDouble(Double(lineWidth),  forKey: Constants.LineWidth)
+    
+    var r:CGFloat = 0
+    var g:CGFloat = 0
+    var b:CGFloat = 0
+    var a:CGFloat = 0
+
+    if lineColor.getRed(&r, green: &g, blue: &b, alpha: &a) {
+      aCoder.encodeDouble(Double(r),  forKey: Constants.ColorRed)
+      aCoder.encodeDouble(Double(g),  forKey: Constants.ColorGreen)
+      aCoder.encodeDouble(Double(b),  forKey: Constants.ColorBlue)
+      aCoder.encodeDouble(Double(a),  forKey: Constants.ColorAlpha)
+    }
+    
   }
   
   var lineWidth: CGFloat {
