2efbaf4d7a65db3b617e5bd4f2e3497227e82504
Brian Young <bdy-github@hiddentower.com>
2015-08-09 14:29:09 -0700

    Added splitViewContoller delegate and method and formatting

--- GeekSpeak Show Timer/SplitViewController.swift
+++ GeekSpeak Show Timer/SplitViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class SplitViewController: UISplitViewController {
+class SplitViewController: UISplitViewController, UISplitViewControllerDelegate {
   
   
   override func viewDidLoad() {
     preferredDisplayMode = UISplitViewControllerDisplayMode.PrimaryOverlay
+    self.delegate = self
   }
   
   // http://stackoverflow.com/questions/26633172/sizing-class-for-ipad-portrait-and-landscape-modes/28268200#28268200
@@ -15,13 +16,25 @@ class SplitViewController: UISplitViewController {
       return UITraitCollection(horizontalSizeClass: .Regular)
     }
   }
-
+  
+  // Make sure the Settings View is displayed on startup
+  // http://stackoverflow.com/questions/25875618/uisplitviewcontroller-in-portrait-on-iphone-shows-detail-vc-instead-of-master
+  func splitViewController(splitViewController: UISplitViewController,
+    collapseSecondaryViewController secondaryViewController: UIViewController!,
+            ontoPrimaryViewController primaryViewController: UIViewController!)
+                                                                      -> Bool {
+    return true
+  }
+  
 }
 
 
 extension UISplitViewController {
   func toggleMasterView() {
     let barButtonItem = self.displayModeButtonItem()
-    UIApplication.sharedApplication().sendAction(barButtonItem.action, to: barButtonItem.target, from: nil, forEvent: nil)
+    UIApplication.sharedApplication().sendAction( barButtonItem.action,
+                                              to: barButtonItem.target,
+                                            from: nil,
+                                        forEvent: nil)
   }
 }
