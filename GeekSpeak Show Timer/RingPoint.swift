17e702e3b79042d54ba3105f69e5914c92e2b7b7
Brian Young <bdy-github@hiddentower.com>
2015-09-14 09:32:23 -0700

    noted the mess that are these classes

--- GeekSpeak Show Timer/RingPoint.swift
+++ GeekSpeak Show Timer/RingPoint.swift
@@ -1,3 +1,19 @@
+// Ring Classes:  Refactor!!!
+//                The classes for drawing and layingout the ring are completely
+//                messing confussing, uses similar but different propery  names
+//                mix obj-c and swift cause type conversions that are confusing
+//                and general a mess.  Clean this mess up!!!
+//                Draw cleanly and be nice.  ;)
+//
+//                  GSTRing.h
+//                  GSTRing.m
+//                  GSTRingLayer.h
+//                  RingCircle.swift
+//                  RingFillView.swift
+//                  RingPoint.swift
+//                  RingView+Progress.swift
+//                  RingView.swift
+
 import Foundation
 
 class RingPoint: NSObject {
