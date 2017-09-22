a2ebcdd0bb309db9043dfe72b095d2aaa2019080
Brian Cordan Young <brian@cordanyoung.com>
2017-09-22 11:42:53 -0700

    Adds playgrounds

---
+++ Messurement Play.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+
+var angle = Measurement(value:2.2345234, unit: UnitAngle(symbol: "°"))
+
+//angle.convert(to: UnitAngle(symbol: "°"))
+
---
+++ Views Play.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: A UIKit based Playground to present user interface
+  
+import UIKit
+import PlaygroundSupport
+//import TimerViewsGear
+
+class myViewController : UIViewController {
+
+    override func loadView() {
+        let view = UIView()
+
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textColor = .white
+        
+        view.addSubview(label)
+        self.view = view
+    }
+   
+}
+
+PlaygroundPage.current.liveView = myViewController()
